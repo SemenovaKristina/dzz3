aad762626aea5b386a8447c75d5893dd38311a5f
Kristina Semenova <semenova.kristina.00@mail.ru>
2021-08-13 16:29:21 +0300

    Added src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 300;
+        int plus = 1400;
+        int sum = start + plus;
+        System.out.println(sum);
+
+        if (plus >=1000){
+            System.out.println(plus / 100);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
